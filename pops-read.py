9e1945f56c04f9d3011b561fdd042917b83e4354
ian <thompson97@llnl.gov>
2021-02-27 19:22:51 -0800

    Some useful misc files: ScalingFactors csh.gpu3 set.gpu3 pops-read.py ripl2pops_to_Z20.xml

---
+++ pops-read.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python
+
+
+from PoPs import database
+import sys
+
+print('Read',sys.argv[1])
+pops = database.database.readFile(sys.argv[1])
+for p in sys.argv[2:]:
+    print('Read',p)
+    pops.addFile(p)
+
+# with open('pops-endf8-global.xml','w') as f:
+#     print(pops.toXML(), file=f)
+print(pops.keys())
+
+maxLevel = {}
+for nuclide in pops.keys():
+    if '_e' not in nuclide or nuclide[0].islower(): continue
+#     print (nuclide)
+    element,level = nuclide.split('_e')
+    level = int(level)
+    
+    if element in maxLevel.keys():
+        maxLevel[element] = max(maxLevel[element], level)
+    else:
+        maxLevel[element] = level
+        
+        
+for element in maxLevel.keys():
+    print('%s max level  %3i' % (element,maxLevel[element]))
+
+
